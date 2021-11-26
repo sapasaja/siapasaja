1e207b6604b471914bbdd56db2cfc6bd9e157a5a
sapasaja <94762995+sapasaja@users.noreply.github.com>
2021-11-27 00:14:33 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -6,7 +6,7 @@ echo ".%%..%%..%%.%%%..%%.%%%..%%..%%............%%....%%..%%..%%%%......%%....%
 echo ".%%..%%..%%..%%..%%..%%..%%..%%............%%....%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
 echo "..%%%%...%%..%%..%%..%%...%%%%.............%%.....%%%%...%%..%%..%%%%%%..%%..%%..%%%%%%..%%..%%...%%%%.."
 echo "........................................................................................................"
-echo                                           "khasbunalloh wanikmal wakil"
+echo                                          "khasbunalloh wanikmal wakil"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
@@ -30,16 +30,16 @@ echo "=========================90%"
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt install xfce4 -y > /dev/null 2>&1
 sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
+echo "==============================95"
+echo "BISMILLAH AYO MULAI MANCING DOLAR"
+echo "=================================100"
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
+echo "====================================="
 echo "===============finish================="
 echo "aja klalen dukung ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ"
 echo "Tab iki aja di close ya soale bisa urip 12 jam"
-echo "===================================="
+echo "====================================="
 sleep 9876543210
