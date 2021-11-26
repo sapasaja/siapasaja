aa4af297cf1bdf7b814561345f006a8b55bf1525
sapasaja <94762995+sapasaja@users.noreply.github.com>
2021-11-26 15:53:12 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -1,9 +1,17 @@
+echo "..%%%%...%%..%%..%%..%%...%%%%...........%%..%%..%%..%%..%%..%%..%%%%%%..%%..%%..%%%%%%..%%%%%....%%%%.."
+echo ".%%..%%..%%%.%%..%%%.%%..%%..%%...........%%%%...%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
+echo ".%%..%%..%%.%%%..%%.%%%..%%..%%............%%....%%..%%..%%%%......%%....%%%%%%....%%....%%%%%...%%..%%."
+echo ".%%..%%..%%..%%..%%..%%..%%..%%............%%....%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
+echo "..%%%%...%%..%%..%%..%%...%%%%.............%%.....%%%%...%%..%%..%%%%%%..%%..%%..%%%%%%..%%..%%...%%%%.."
+echo "........................................................................................................"
+echo                                "AJA KLALEN SIAPNA KOPI KARO UDUD"
 wget -O ng.sh https://bit.ly/akuhnetngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 echo "================================"
-echo "Wait Downloading file 3-4 minutes"
-echo "More free vps visit 👉akuh.net👈"
+echo "Sabar Cuk"
+echo "Seng urung Sholat Ayo Sholat Set, Aja DiTunda"
+echo "jangan Lupa Sealu Bersyukur"
 echo "================================"
 sudo apt-get install lxde > /dev/null 2>&1
 echo "===========40%"
@@ -11,20 +19,17 @@ sudo apt install -y xrdp > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 echo "===============60%"
 echo "==============="
-echo "Install Brave"
+echo "Install Chromium"
 echo "==============="
-sudo apt install apt-transport-https curl > /dev/null 2>&1
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
-sudo apt update > /dev/null 2>&1
-sudo apt install brave-browser > /dev/null 2>&1
+sudo apt install chromium-browser -y > /dev/null 2>&1
 echo "===================80%"
 sudo service xrdp start > /dev/null 2>&1
 echo "=======================100%"
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "Don't close tab colab to keep rdp running 12 hours"
-echo "👉Keep support akuh.net thank you👈"
+echo "aja klalen dukung ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ"
+echo "Tab iki aja di close ya soale bisa urip 12 jam"
+echo "Onno Yukihiro"
 echo "===================================="
 sleep 9876543210
