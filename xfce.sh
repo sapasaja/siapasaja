8ca73daba60e1e6679c85e221d21d427b9f7cb8b
sapasaja <94762995+sapasaja@users.noreply.github.com>
2021-11-29 06:04:04 +0700

    Create xfce.sh

---
+++ xfce.sh
@@ -0,0 +1,48 @@
+echo ".............................................❖﷽❖..........................................."
+echo "........................................................................................................"
+echo "..%%%%...%%..%%..%%..%%...%%%%...........%%..%%..%%..%%..%%..%%..%%%%%%..%%..%%..%%%%%%..%%%%%....%%%%.."
+echo ".%%..%%..%%%.%%..%%%.%%..%%..%%...........%%%%...%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
+echo ".%%..%%..%%.%%%..%%.%%%..%%..%%............%%....%%..%%..%%%%......%%....%%%%%%....%%....%%%%%...%%..%%."
+echo ".%%..%%..%%..%%..%%..%%..%%..%%............%%....%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
+echo "..%%%%...%%..%%..%%..%%...%%%%.............%%.....%%%%...%%..%%..%%%%%%..%%..%%..%%%%%%..%%..%%...%%%%.."
+echo "........................................................................................................"
+echo                                          "khasbunalloh wanikmal wakil"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+echo ꦣꦺꦴꦮ꧀ꦤꦮꦢ꧀​ꦥ꦳ꦶꦊ​ꦱꦏꦶꦁ​ ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ
+echo "Sabar"
+echo "Seng urung Sholat Ayo Sholat NdiSit, Aja DiTunda"
+echo "ꦱꦼꦏꦼꦢꦥ꧀​ꦚ꧀ꦗꦶꦃ"
+echo "ꦠꦱꦶꦃ​ꦭꦺꦴꦮꦢꦶꦁ​..."
+echo "■0%"
+echo "▰▰10%"
+echo "▰▰▰▰20%"
+echo "▰▰▰▰▰30%"
+echo "▰▰▰▰▰▰40%"
+sudo apt-get update > /dev/null 2>&1
+echo "▰▰▰▰▰▰▰▰60%"
+sudo apt install chromium-browser -y > /dev/null 2>&1
+echo "▰▰▰▰▰▰▰▰▰▰80%"
+echo "▰▰▰▰▰▰▰▰▰▰▰▰90%"
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰95"
+echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰100"
+echo "BISMILLAH AYO MULAI MANCING DOLAR"
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./chrome-remote-desktop_current_amd64.deb
+mkdir ~/.config/chrome-remote-desktop
+echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰..finish"
+echo "aja klalen dukung ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ"
+echo "Tab iki aja di close ya soale bisa urip 12 jam"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 9876543210
