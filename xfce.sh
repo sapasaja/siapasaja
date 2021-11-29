51255edd9615512e0bec31efb0c21528079caa1c
sapasaja <94762995+sapasaja@users.noreply.github.com>
2021-11-29 15:00:04 +0700

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -38,9 +38,6 @@ sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install ./chrome-remote-desktop_current_amd64.deb
-mkdir ~/.config/chrome-remote-desktop
 echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰..finish"
 echo "aja klalen dukung ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ"
 echo "Tab iki aja di close ya soale bisa urip 12 jam"
