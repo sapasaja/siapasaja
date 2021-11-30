d3a0d954513a894f1f98008cabcc2266dd913b79
sapasaja <94762995+sapasaja@users.noreply.github.com>
2021-11-30 09:40:41 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -20,23 +20,23 @@ echo "ꦱꦼꦏꦼꦢꦥ꧀​ꦚ꧀ꦗꦶꦃ"
 echo "ꦠꦱꦶꦃ​ꦭꦺꦴꦮꦢꦶꦁ​..."
 echo "■0%"
 echo "▰▰10%"
-echo "▰▰▰▰20%"
-echo "▰▰▰▰▰30%"
-echo "▰▰▰▰▰▰40%"
 sudo apt-get update > /dev/null 2>&1
-echo "▰▰▰▰▰▰▰▰60%"
+echo "▰▰▰▰20%"
 wget wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+echo "▰▰▰▰▰30%"
 sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1
-echo "▰▰▰▰▰▰▰▰▰▰80%"
-echo "▰▰▰▰▰▰▰▰▰▰▰▰90%"
+echo "▰▰▰▰▰▰40%"
 sudo apt install -y xrdp > /dev/null 2>&1
+echo "▰▰▰▰▰▰▰▰60%"
 sudo apt install xfce4 -y > /dev/null 2>&1
+echo "▰▰▰▰▰▰▰▰▰▰80%"
 sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+echo "▰▰▰▰▰▰▰▰▰▰▰▰90%"
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰95"
+sudo service xrdp start > /dev/null 2>&1
 echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰100"
 echo "BISMILLAH AYO MULAI MANCING DOLAR"
-sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰..finish"
