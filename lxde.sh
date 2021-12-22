cd23627ec17321d08fc575a7f88e09637d1f74e4
sapasaja <94762995+sapasaja@users.noreply.github.com>
2021-12-23 02:04:29 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -1,21 +1,35 @@
-echo "..%%%%...%%..%%..%%..%%...%%%%...........%%..%%..%%..%%..%%..%%..%%%%%%..%%..%%..%%%%%%..%%%%%....%%%%.."
-echo ".%%..%%..%%%.%%..%%%.%%..%%..%%...........%%%%...%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
-echo ".%%..%%..%%.%%%..%%.%%%..%%..%%............%%....%%..%%..%%%%......%%....%%%%%%....%%....%%%%%...%%..%%."
-echo ".%%..%%..%%..%%..%%..%%..%%..%%............%%....%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
-echo "..%%%%...%%..%%..%%..%%...%%%%.............%%.....%%%%...%%..%%..%%%%%%..%%..%%..%%%%%%..%%..%%...%%%%.."
+echo ".............................................❖﷽❖..........................................."
+echo                                          "khasbunalloh wanikmal wakil"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+echo ꦣꦺꦴꦮ꧀ꦤꦮꦢ꧀​ꦥ꦳ꦶꦊ​ꦱꦏꦶꦁ​ ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ
+echo "Sabar"
+echo "Seng urung Sholat Ayo Sholat NdiSit, Aja DiTunda"
+echo "ꦱꦼꦏꦼꦢꦥ꧀​ꦚ꧀ꦗꦶꦃ"
+echo "ꦠꦱꦶꦃ​ꦭꦺꦴꦮꦢꦶꦁ​..."
 echo "........................................................................................................"
-wget -O ng.sh https://bit.ly/akuhnetngrok > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
-echo "Seng urung Sholat Ayo Sholat Set, Aja DiTunda"
-echo "jangan Lupa Sealu Bersyukur"
-udo apt-get update > /dev/null 2>&1
-sudo apt install chromium-browser -y > /dev/null 2>&1
+sudo apt-get update > /dev/null 2>&1
+echo "..%%%%...%%..%%..%%..%%...%%%%...........%%..%%..%%..%%..%%..%%..%%%%%%..%%..%%..%%%%%%..%%%%%....%%%%.."
+sudo apt install firefox -y > /dev/null 2>&1
+echo ".%%..%%..%%%.%%..%%%.%%..%%..%%...........%%%%...%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
 sudo apt install -y xrdp > /dev/null 2>&1
-sudo apt-get install lxde > /dev/null 2>&1
+echo ".%%..%%..%%.%%%..%%.%%%..%%..%%............%%....%%..%%..%%%%......%%....%%%%%%....%%....%%%%%...%%..%%."
+sudo apt install lxde -y > /dev/null 2>&1
+echo ".%%..%%..%%..%%..%%..%%..%%..%%............%%....%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
 sudo apt-get install lxde lxde-terminal -y > /dev/null 2>&1
-sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "..%%%%...%%..%%..%%..%%...%%%%.............%%.....%%%%...%%..%%..%%%%%%..%%..%%..%%%%%%..%%..%%...%%%%.."
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
+echo "........................................................................................................"
+echo "BISMILLAH AYO MULAI MANCING DOLAR"
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰..finish"
+echo "aja klalen dukung ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ"
+echo "Tab iki aja di close ya soale bisa urip 12 jam"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
 sleep 9876543210
