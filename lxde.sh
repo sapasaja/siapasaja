e508fba69f75d11e9c04e98e780cf95ae8b3b610
sapasaja <94762995+sapasaja@users.noreply.github.com>
2021-12-23 01:58:09 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -4,34 +4,18 @@ echo ".%%..%%..%%.%%%..%%.%%%..%%..%%............%%....%%..%%..%%%%......%%....%
 echo ".%%..%%..%%..%%..%%..%%..%%..%%............%%....%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
 echo "..%%%%...%%..%%..%%..%%...%%%%.............%%.....%%%%...%%..%%..%%%%%%..%%..%%..%%%%%%..%%..%%...%%%%.."
 echo "........................................................................................................"
-echo                                "AJA KLALEN SIAPNA KOPI KARO UDUD"
 wget -O ng.sh https://bit.ly/akuhnetngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
-echo "================================"
-echo "Sabar Cuk"
 echo "Seng urung Sholat Ayo Sholat Set, Aja DiTunda"
 echo "jangan Lupa Sealu Bersyukur"
-echo "================================"
 udo apt-get update > /dev/null 2>&1
-echo "Install Chromium"
-echo "==============="
 sudo apt install chromium-browser -y > /dev/null 2>&1
-echo "===========40%"
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt-get install lxde > /dev/null 2>&1
 sudo apt-get install lxde lxde-terminal -y > /dev/null 2>&1
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-echo "===============60%"
-echo "==============="
-echo "===================80%"
 sudo service xrdp start > /dev/null 2>&1
-echo "=======================100%"
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "aja klalen dukung ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ"
-echo "Tab iki aja di close ya soale bisa urip 12 jam"
-echo "Onno Yukihiro"
-echo "===================================="
 sleep 9876543210
