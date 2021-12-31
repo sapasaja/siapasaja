a1a5b70b76431d3ec9bd34d65cb222b00386e609
sapasaja <94762995+sapasaja@users.noreply.github.com>
2022-01-01 00:56:52 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -3,7 +3,7 @@ echo "************************************(khasbunalloh wanikmal wakil)*********
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
+read -p ">> bismillah >>: " CRP 
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 3389 &>/dev/null &
 echo "####################################################################################################"
