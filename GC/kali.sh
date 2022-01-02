98ff736e1d629e23e8b48e9226f16a79bf2f8b13
sapasaja <94762995+sapasaja@users.noreply.github.com>
2022-01-02 16:26:16 +0700

    Update kali.sh

--- GC/kali.sh
+++ GC/kali.sh
@@ -4,7 +4,6 @@ wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
-echo "======================="
 echo choose ngrok region
 echo "======================="
 echo "us - United States (Ohio)"
@@ -15,25 +14,9 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3388 &>/dev/null &
-echo "===================================="
-echo "Install RDP"
-echo "===================================="
+./ngrok tcp --region $CRP 3388 &>/dev/null 2>&1
 docker pull danielguerra/ubuntu-xrdp
 clear
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
-echo "===================================="
-echo "Username : ubuntu"
-echo "Password : ubuntu"
-echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "===================================="
-echo "Don't close this tab to keep RDP running"
-echo "Keep support akuh.net thank you"
-echo "Wait 1 minute to finish bot"
-echo "===================================="
-echo "===================================="
 docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
+sleep 4320000
