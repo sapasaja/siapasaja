64d6a19e596d16f1dc278d98f2c4bbdf8a5b0084
sapasaja <94762995+sapasaja@users.noreply.github.com>
2022-01-02 16:31:19 +0700

    Update GCngrok.sh

--- GC/GCngrok.sh
+++ GC/GCngrok.sh
@@ -1,8 +1,5 @@
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
-echo "======================="
-echo "Download ngrok"
-echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: " CRP
+read -p ">>bismillah>>: " CRP
 ./ngrok authtoken $CRP 
