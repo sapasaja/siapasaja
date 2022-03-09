de21c31f3f08d903a2feac435c26402a6892d0f2
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-10 01:57:42 +0700

    Update andro.sh

--- andro.sh
+++ andro.sh
@@ -1,12 +1,14 @@
 apt-get update
-wget -O 3HYnejd.img https://bit.ly/3HYnejd
+wget -O android.iso https://bit.ly/3CuFXl1
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p ">>bismillah>>: " CRP 
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 3389 &>/dev/null &
 apt-get install qemu
+apt-get install qemu-kvm qemu
+qemu-img create andro.img 30G
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-qemu-system-x86_64 -hda 3HYnejd.img -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+qemu-system-x86_64 -hda android.img -m 8G ,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
 sleep 4320000
