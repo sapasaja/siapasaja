c937de99c3b37716f8dd347c7254895c10859edb
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-03-10 21:17:48 +0700

    Update andro.sh

--- andro.sh
+++ andro.sh
@@ -1,14 +1,14 @@
 apt-get update
-wget -O android.iso https://bit.ly/3CuFXl1
+wget -O android.vdi https://telkomuniversity.dl.sourceforge.net/project/osboxes/v/vb/1-A-d/9.0/R2/andr-9.0-r2-VB-64bit.7z
+apt install p7zip -y > /dev/null 2>&1
+7z x android.7z
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p ">>bismillah>>: " CRP 
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 3389 &>/dev/null &
+nohup ./ngrok tcp 3000 &>/dev/null &
 apt-get install qemu
-apt-get install qemu-kvm qemu
-qemu-img create andro.img 30G
-echo RDP Address:
+echo No Machine Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-qemu-system-x86_64 -hda andro.img -m 8G ,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
+qemu-system-x86_64 -hda android.vdi -m 8G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic
 sleep 4320000
