26652706ed28030ea4880ee584913262ce185cdb
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-04-01 16:54:26 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -10,6 +10,8 @@ echo "##########################################################################
 sudo apt update > /dev/null 2>&1
 echo "#..%%%%...%%..%%..%%..%%...%%%%.....%%..%%..%%..%%..%%..%%..%%..%%..%%..%%..%%%%%....%%%%..#"
 sudo apt install firefox -y > /dev/null 2>&1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo apt install ./google-chrome-stable_current_amd64.deb -y > /dev/null 2>&1
 echo "#.%%..%%..%%%.%%..%%%.%%..%%..%%.....%%%%...%%..%%..%%.%%...%%..%%..%%..%%..%%..%%..%%..%%.#"
 sudo apt install -y xrdp > /dev/null 2>&1
 echo "#.%%..%%..%%.%%%..%%.%%%..%%..%%......%%....%%..%%..%%%%....%%..%%%%%%..%%..%%%%%...%%..%%.#"
