287890f0607f89befe16d329c9af482787c171ca
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-04-03 16:55:01 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -2,7 +2,6 @@ echo "####################################❤❖﷽❖❤#######################
 echo "********************************(khasbunalloh wanikmal wakil)*******************************"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
 read -p ">> bismillah >>: " CRP 
 ./ngrok authtoken $CRP &>/dev/null &
 nohup ./ngrok tcp 3389 &>/dev/null &
