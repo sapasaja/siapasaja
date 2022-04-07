8dd3890416590f4436374c2d65b6db64507b6629
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-04-08 00:49:53 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -6,11 +6,7 @@ read -p ">> bismillah >>: " CRP
 ./ngrok authtoken $CRP &>/dev/null &
 nohup ./ngrok tcp 3389 &>/dev/null &
 echo "############################################################################################"
-sudo apt install apt-transport-https curl > /dev/null 2>&1
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
 sudo apt update > /dev/null 2>&1
-sudo apt install brave-browser > /dev/null 2>&1
 echo "#..%%%%...%%..%%..%%..%%...%%%%.....%%..%%..%%..%%..%%..%%..%%..%%..%%..%%..%%%%%....%%%%..#"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
 sudo apt install ./google-chrome-stable_current_amd64.deb -y > /dev/null 2>&1
