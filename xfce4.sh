7d37a7d529a42a6020b03f93aa0987184564c831
kulit_kuwaci <94762995+sapasaja@users.noreply.github.com>
2022-07-21 02:01:26 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -1,35 +1,26 @@
-echo ".............................................❖﷽❖..........................................."
-echo                                          "khasbunalloh wanikmal wakil"
+echo "####################################❤❖﷽❖❤#####################################"
+echo "********************************(khasbunalloh wanikmal wakil)*******************************"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
-read -p "Ctrl + V Authtoken: " CRP 
-./ngrok authtoken $CRP 
+read -p ">> bismillah >>: " CRP 
+./ngrok authtoken $CRP &>/dev/null &
 nohup ./ngrok tcp 3389 &>/dev/null &
-echo ꦣꦺꦴꦮ꧀ꦤꦮꦢ꧀​ꦥ꦳ꦶꦊ​ꦱꦏꦶꦁ​ ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ
-echo "Sabar"
-echo "Seng urung Sholat Ayo Sholat NdiSit, Aja DiTunda"
-echo "ꦱꦼꦏꦼꦢꦥ꧀​ꦚ꧀ꦗꦶꦃ"
-echo "ꦠꦱꦶꦃ​ꦭꦺꦴꦮꦢꦶꦁ​..."
-echo "........................................................................................................"
+echo "############################################################################################"
 sudo apt-get update > /dev/null 2>&1
 echo "..%%%%...%%..%%..%%..%%...%%%%...........%%..%%..%%..%%..%%..%%..%%%%%%..%%..%%..%%%%%%..%%%%%....%%%%.."
 sudo apt install firefox -y > /dev/null 2>&1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1
+sudo apt install ./google-chrome-stable_current_amd64.deb -y > /dev/null 2>&1
 echo ".%%..%%..%%%.%%..%%%.%%..%%..%%...........%%%%...%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
 sudo apt install -y xrdp > /dev/null 2>&1
 echo ".%%..%%..%%.%%%..%%.%%%..%%..%%............%%....%%..%%..%%%%......%%....%%%%%%....%%....%%%%%...%%..%%."
 sudo apt install xfce4 -y > /dev/null 2>&1
 echo ".%%..%%..%%..%%..%%..%%..%%..%%............%%....%%..%%..%%.%%.....%%....%%..%%....%%....%%..%%..%%..%%."
-sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+sudo apt-get install mate mate-terminal -y > /dev/null 2>&1
 echo "..%%%%...%%..%%..%%..%%...%%%%.............%%.....%%%%...%%..%%..%%%%%%..%%..%%..%%%%%%..%%..%%...%%%%.."
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
-echo "........................................................................................................"
-echo "BISMILLAH AYO MULAI MANCING DOLAR"
-echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰..finish"
-echo "aja klalen dukung ꦲꦺꦴꦤ꧀ꦤꦺꦴ​ꦪꦸꦏꦶꦲꦶꦫꦺꦴ"
-echo "Tab iki aja di close ya soale bisa urip 12 jam"
-echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "*****************************©2022 - 🅞🅝🅝🅞 🅨🅤🅚🅘🅗🅘🅡🅞***********************************"
+echo "###################❤سُبْحَانَ اللهِ، وَالْحَمْدُ لِلَّهِ، وَلَا إِلَهَ إِلَّا اللهُ، وَاللهُ أَكْبَرُ، وَلَا حَوْلَ وَلَا قُوَّةَ إِلَّا بِاللهِ❤####################"
 sleep 9876543210
